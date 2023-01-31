66e6e66fe99fa69f104ea500ef19c2fea6b3bc2c
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-01-31 16:14:47 +0200

    Added @Table name

--- src/main/java/carsapp/carsbuysell/models/entities/Brand.java
+++ src/main/java/carsapp/carsbuysell/models/entities/Brand.java
@@ -2,10 +2,12 @@ package carsapp.carsbuysell.models.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "brands")
 public class Brand extends BaseEntity {
     @Column(nullable = false)
     private String name;
--- src/main/java/carsapp/carsbuysell/models/entities/User.java
+++ src/main/java/carsapp/carsbuysell/models/entities/User.java
@@ -2,14 +2,16 @@ package carsapp.carsbuysell.models.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "users")
 public class User extends BaseEntity {
-    @Column(nullable = false)
+    @Column(nullable = false,unique = true)
     private String username;
-    @Column(nullable = false)
+    @Column(nullable = false,unique = true)
     private String email;
     @Column(nullable = false)
     private String password;
