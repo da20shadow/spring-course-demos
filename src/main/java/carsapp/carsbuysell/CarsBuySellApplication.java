22b830ef6f669200141f647d5f32f2c1d923121a
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-01-31 15:53:39 +0200

    Initial Commit

---
+++ src/main/java/carsapp/carsbuysell/CarsBuySellApplication.java
@@ -0,0 +1,13 @@
+package carsapp.carsbuysell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsBuySellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarsBuySellApplication.class, args);
+    }
+
+}
---
+++ src/test/java/carsapp/carsbuysell/CarsBuySellApplicationTests.java
@@ -0,0 +1,13 @@
+package carsapp.carsbuysell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsBuySellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
