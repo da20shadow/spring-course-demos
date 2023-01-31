78ac176210ce1e6e19afe8d966ee91309fa675ce
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-01-31 23:34:37 +0200

    Created UserRegisterDTO

---
+++ src/main/java/carsapp/carsbuysell/models/dtos/UserRegisterDTO.java
@@ -0,0 +1,54 @@
+package carsapp.carsbuysell.models.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public class UserRegisterDTO {
+    @NotEmpty(message = "Username can not be empty!")
+    private String username;
+    @NotEmpty(message = "Email can not be empty!")
+    @Email(message = "Invalid email!")
+    private String email;
+    @NotEmpty
+    @Size(min = 5)
+    private String password;
+    @NotEmpty
+    @Size(min = 5)
+    private String confirmPassword;
+
+    public UserRegisterDTO() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
