84d2ab8059940e14b280b4bbc9bea55e75c72ec0
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-01-31 16:09:17 +0200

    Created User entity

---
+++ src/main/java/carsapp/carsbuysell/models/entities/User.java
@@ -0,0 +1,103 @@
+package carsapp.carsbuysell.models.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class User extends BaseEntity {
+    @Column(nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "is_active")
+    private boolean isActive;
+    @Column(name = "avatar_img_url")
+    private String avatarImgUrl;
+    @Column
+    private LocalDateTime created;
+    @Column
+    private LocalDateTime modified;
+
+    public User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public String getAvatarImgUrl() {
+        return avatarImgUrl;
+    }
+
+    public void setAvatarImgUrl(String avatarImgUrl) {
+        this.avatarImgUrl = avatarImgUrl;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public LocalDateTime getModified() {
+        return modified;
+    }
+
+    public void setModified(LocalDateTime modified) {
+        this.modified = modified;
+    }
+}
