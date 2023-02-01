c8d73c378c17bbe2ba98c8ba557da5f74ec9a725
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-01 16:33:48 +0200

    added methods

--- src/main/java/carsapp/carsbuysell/services/user/UserServiceImpl.java
+++ src/main/java/carsapp/carsbuysell/services/user/UserServiceImpl.java
@@ -1,9 +1,12 @@
 package carsapp.carsbuysell.services.user;
 
+import carsapp.carsbuysell.models.entities.User;
 import carsapp.carsbuysell.repositories.UserRepository;
 import carsapp.carsbuysell.services.init.DbInitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService, DbInitService {
@@ -23,5 +26,21 @@ public class UserServiceImpl implements UserService, DbInitService {
     @Override
     public boolean isDbInitialized() {
         return this.userRepository.count() > 0;
+    }
+
+    public String register(User user){
+        var result = this.userRepository.save(user);
+        System.out.println(result.getUsername());
+        return "Successfully Registered!";
+    }
+
+    @Override
+    public boolean findOneByUsername(String username) {
+        return this.userRepository.findByUsername(username) != null;
+    }
+
+    @Override
+    public List<User> getAll() {
+        return userRepository.findAll();
     }
 }
