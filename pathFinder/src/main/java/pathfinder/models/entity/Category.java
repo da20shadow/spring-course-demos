c90ebe376233e35f8f6daa71fcf896e0576c4e14
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-02 17:02:03 +0200

    Created categories

---
+++ pathFinder/src/main/java/pathfinder/models/entity/Category.java
@@ -0,0 +1,34 @@
+package pathfinder.models.entity;
+
+import jakarta.persistence.*;
+import pathfinder.models.enums.CategoryName;
+
+@Entity
+@Table(name = "categories")
+public class Category extends BaseEntity{
+    @Enumerated(EnumType.STRING)
+    private CategoryName name;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    public Category() {
+    }
+
+    public CategoryName getName() {
+        return name;
+    }
+
+    public Category setName(CategoryName name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Category setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+}
---
+++ pathFinder/src/main/java/pathfinder/models/enums/CategoryName.java
@@ -0,0 +1,5 @@
+package pathfinder.models.enums;
+
+public enum CategoryName {
+    PEDESTRIAN, BICYCLE, MOTORCYCLE, CAR
+}
