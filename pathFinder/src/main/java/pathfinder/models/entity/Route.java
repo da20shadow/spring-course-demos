1dc61bf0915d8f06bef27f5a5404194fb46f4437
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-02 16:55:44 +0200

    Create route entity

---
+++ pathFinder/src/main/java/pathfinder/models/entity/Route.java
@@ -0,0 +1,67 @@
+package pathfinder.models.entity;
+
+import jakarta.persistence.*;
+import pathfinder.models.enums.LEVEL;
+
+@Entity
+@Table(name = "routes")
+public class Route extends BaseEntity{
+    @Column(columnDefinition = "TEXT")
+    private String gpx;
+    @Enumerated(EnumType.STRING)
+    private LEVEL level;
+    @Column
+    private String name;
+    @ManyToOne
+    private User author;
+    @Column
+    private String url;
+
+    public Route() {
+    }
+
+    public String getGpx() {
+        return gpx;
+    }
+
+    public Route setGpx(String gpx) {
+        this.gpx = gpx;
+        return this;
+    }
+
+    public LEVEL getLevel() {
+        return level;
+    }
+
+    public Route setLevel(LEVEL level) {
+        this.level = level;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Route setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public Route setAuthor(User author) {
+        this.author = author;
+        return this;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Route setUrl(String url) {
+        this.url = url;
+        return this;
+    }
+}
