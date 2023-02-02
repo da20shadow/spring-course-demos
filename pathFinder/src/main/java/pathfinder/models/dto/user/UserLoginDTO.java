3044739290c2cc3a1ab543b56453015b0f1fca34
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-02 15:06:23 +0200

    Create user login dto

---
+++ pathFinder/src/main/java/pathfinder/models/dto/user/UserLoginDTO.java
@@ -0,0 +1,32 @@
+package pathfinder.models.dto.user;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public class UserLoginDTO {
+    @NotEmpty
+    @Size(min = 5,max = 45)
+    private String username;
+    @NotEmpty
+    @Size(min = 8,max = 45)
+    private String password;
+
+    public UserLoginDTO() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
