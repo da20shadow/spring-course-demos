a65fc37e0817e6fdc09bf68cb3b442dd8078c1df
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-02 16:43:08 +0200

    edit profile dto

---
+++ pathFinder/src/main/java/pathfinder/models/dto/user/EditProfileDTO.java
@@ -0,0 +1,34 @@
+package pathfinder.models.dto.user;
+
+public class EditProfileDTO {
+    private String email;
+    private String age;
+    private String password;
+
+    public EditProfileDTO() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
