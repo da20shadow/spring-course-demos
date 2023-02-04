eec215f8baff31beec93f4fd298d83d7eea488f1
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-04 19:44:28 +0200

    add most commented route in homepage

--- pathFinder/src/main/java/pathfinder/controllers/StaticPageController.java
+++ pathFinder/src/main/java/pathfinder/controllers/StaticPageController.java
@@ -1,12 +1,31 @@
 package pathfinder.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pathfinder.models.dto.route.view.MostCommentedRouteViewDTO;
+import pathfinder.services.RouteService;
 
 @Controller
 @RequestMapping("/")
 public class StaticPageController {
+
+    private final RouteService routeService;
+
+    @Autowired
+    public StaticPageController(RouteService routeService) {
+        this.routeService = routeService;
+    }
+
+    @GetMapping
+    public String getHomePage(Model model){
+        final MostCommentedRouteViewDTO mostCommentedRouteViewDTO =
+                routeService.getMostCommented();
+        model.addAttribute("mostCommented", mostCommentedRouteViewDTO);
+        return "index";
+    }
 
     @GetMapping("about")
     public String getAboutPage(){
