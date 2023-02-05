348cad631a03d2b33d500441e4b5d921ea1f2fd9
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-05 23:42:00 +0200

    Create add route dto

---
+++ pathFinder/src/main/java/pathfinder/models/dto/route/AddRouteDTO.java
@@ -0,0 +1,13 @@
+package pathfinder.models.dto.route;
+
+public class AddRouteDTO {
+    private String name;
+    private String description;
+    private String gpx;
+    private String level;
+    private String videoUrl;
+    private String pedestrian;
+    private String bicycle;
+    private String motorcycle;
+    private String car;
+}
