193af1d492d7a783d0e29668f1831f91e06072d6
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-07 19:03:13 +0200

    added bean for logged user

--- pathFinder/src/main/java/pathfinder/config/BeanConfiguration.java
+++ pathFinder/src/main/java/pathfinder/config/BeanConfiguration.java
@@ -3,6 +3,8 @@ package pathfinder.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.annotation.SessionScope;
+import pathfinder.beans.LoggedUser;
 
 @Configuration
 public class BeanConfiguration {
@@ -10,4 +12,11 @@ public class BeanConfiguration {
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
+
+    @Bean
+    @SessionScope
+    public LoggedUser loggedUser(){
+        return new LoggedUser();
+    }
+
 }
