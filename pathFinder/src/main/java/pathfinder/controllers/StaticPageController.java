6627ce6e2ccb43eb23dfa9a1b162eb39b012581f
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-07 19:04:11 +0200

    updated method added user from cookie
    Added most commented route and setting user from cookie

--- pathFinder/src/main/java/pathfinder/controllers/StaticPageController.java
+++ pathFinder/src/main/java/pathfinder/controllers/StaticPageController.java
@@ -3,9 +3,11 @@ package pathfinder.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pathfinder.models.dto.route.view.MostCommentedRouteViewDTO;
+import pathfinder.models.entity.User;
 import pathfinder.services.RouteService;
 
 @Controller
@@ -20,15 +22,37 @@ public class StaticPageController {
     }
 
     @GetMapping
-    public String getHomePage(Model model){
-        final MostCommentedRouteViewDTO mostCommentedRouteViewDTO =
-                routeService.getMostCommented();
-        model.addAttribute("mostCommented", mostCommentedRouteViewDTO);
+    public String getHomePage(Model model,
+                              @CookieValue(name = "username",defaultValue = "")
+                                      String username) {
+        if (username.equals("")){
+            return "redirect:login";
+        }
+
+        model.addAttribute("user", username);
+
+        try {
+            final MostCommentedRouteViewDTO mostCommentedRouteViewDTO =
+                    routeService.getMostCommented();
+            model.addAttribute("mostCommented", mostCommentedRouteViewDTO);
+            System.out.println(mostCommentedRouteViewDTO.getName());
+            System.out.println(mostCommentedRouteViewDTO.getDescription());
+        } catch (Exception exception) {
+            model.addAttribute("error", "No Routes yet!");
+            System.out.println("No Routes Yet!");
+        }
         return "index";
     }
 
     @GetMapping("about")
-    public String getAboutPage(){
+    public String getAboutPage(Model model,
+                               @CookieValue(name = "username",defaultValue = "")
+                                       String username) {
+        if (username.equals("")){
+            return "redirect:login";
+        }
+
+        model.addAttribute("user", username);
         return "about";
     }
 }
