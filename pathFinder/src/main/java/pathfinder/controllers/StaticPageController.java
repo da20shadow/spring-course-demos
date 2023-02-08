ed1ee34a97a65820c47773bd3d14c8478da81ddc
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-08 13:58:32 +0200

    updated static page controller

--- pathFinder/src/main/java/pathfinder/controllers/StaticPageController.java
+++ pathFinder/src/main/java/pathfinder/controllers/StaticPageController.java
@@ -5,31 +5,33 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pathfinder.models.dto.route.view.MostCommentedRouteViewDTO;
 import pathfinder.models.entity.User;
 import pathfinder.services.RouteService;
+import pathfinder.services.UserService;
+
+import java.util.Date;
 
 @Controller
 @RequestMapping("/")
 public class StaticPageController {
 
     private final RouteService routeService;
+    private final UserService userService;
 
     @Autowired
-    public StaticPageController(RouteService routeService) {
+    public StaticPageController(RouteService routeService, UserService userService) {
         this.routeService = routeService;
+        this.userService = userService;
     }
 
     @GetMapping
-    public String getHomePage(Model model,
-                              @CookieValue(name = "username",defaultValue = "")
-                                      String username) {
-        if (username.equals("")){
-            return "redirect:login";
-        }
+    public String getHomePage(Model model) {
 
-        model.addAttribute("user", username);
+        Date todayDate = new Date();
+        model.addAttribute("todayDate", todayDate);
 
         try {
             final MostCommentedRouteViewDTO mostCommentedRouteViewDTO =
@@ -45,14 +47,17 @@ public class StaticPageController {
     }
 
     @GetMapping("about")
-    public String getAboutPage(Model model,
-                               @CookieValue(name = "username",defaultValue = "")
-                                       String username) {
-        if (username.equals("")){
-            return "redirect:login";
-        }
-
-        model.addAttribute("user", username);
+    public String getAboutPage(Model model) {
         return "about";
+    }
+
+    //Model Attributes
+    @ModelAttribute(name = "user")
+    public User getLoggedUser(@CookieValue(name = "username",defaultValue = "")
+                                      String username){
+        if (username.equals("")){
+            return null;
+        }
+        return this.userService.getUserByUsername(username);
     }
 }
