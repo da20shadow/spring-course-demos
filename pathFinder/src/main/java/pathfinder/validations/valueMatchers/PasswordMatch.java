5cb6fe178221bd8769b7bd358e2c28726aa7f454
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-08 13:57:38 +0200

    create password matcher annotation validator

---
+++ pathFinder/src/main/java/pathfinder/validations/valueMatchers/PasswordMatch.java
@@ -0,0 +1,20 @@
+package pathfinder.validations.valueMatchers;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Constraint(validatedBy = PasswordMatcher.class)
+public @interface PasswordMatch {
+    String password();
+    String confirmPassword();
+    String message() default "Password doesn't match!";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
