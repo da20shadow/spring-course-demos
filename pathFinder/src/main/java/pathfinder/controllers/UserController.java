8c3e80b98dc08b8801c2f3364321fe15368cdd9b
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-08 13:58:23 +0200

    update user controller

--- pathFinder/src/main/java/pathfinder/controllers/UserController.java
+++ pathFinder/src/main/java/pathfinder/controllers/UserController.java
@@ -2,13 +2,16 @@ package pathfinder.controllers;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pathfinder.models.dto.user.EditProfileDTO;
 import pathfinder.models.dto.user.UserLoginDTO;
 import pathfinder.models.dto.user.UserRegisterDTO;
@@ -26,16 +29,24 @@ public class UserController {
     }
 
     @GetMapping("/login")
-    private String getLoginPage(Model model) {
+    private String getLoginPage(@ModelAttribute("userLoginForm") UserLoginDTO userLoginForm,
+                                Model model, @CookieValue(name = "username",defaultValue = "")
+            String username) {
+
+        if (!username.equals("")){
+            model.addAttribute("user", username);
+            return "redirect:profile";
+        }
         model.addAttribute("error", "");
         return "login";
     }
 
     @PostMapping("/login")
     private String postLoginPage(HttpServletResponse response,
-                                 @ModelAttribute UserLoginDTO userLoginDTO, Model model) {
+                                 @ModelAttribute("userLoginForm") UserLoginDTO userLoginForm,
+                                 Model model) {
 
-        var result = this.userService.login(userLoginDTO.getUsername(),userLoginDTO.getPassword());
+        var result = this.userService.login(userLoginForm.getUsername(),userLoginForm.getPassword());
 
         if (result == null){
             model.addAttribute("error", "Bad credentials!");
@@ -55,17 +66,24 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    private String postRegisterPage(@ModelAttribute UserRegisterDTO userRegisterDTO,Model model) {
+    private String postRegisterPage(@Valid @ModelAttribute("userRegForm") UserRegisterDTO userRegForm,
+                                    Model model, BindingResult bindingResult,
+                                    RedirectAttributes redirectAttributes) {
 
-        var resultUsername = this.userService.getUserByUsername(userRegisterDTO.getUsername());
-        var resultEmail = this.userService.getUserByEmail(userRegisterDTO.getEmail());
+        if (bindingResult.hasErrors()){
+            redirectAttributes.addFlashAttribute("userRegForm",userRegForm)
+                    .addFlashAttribute("org.springframework.validation.BindingResult.userRegForm",bindingResult);
+        }
+
+        var resultUsername = this.userService.getUserByUsername(userRegForm.getUsername());
+        var resultEmail = this.userService.getUserByEmail(userRegForm.getEmail());
 
         if (resultUsername != null || resultEmail != null){
             model.addAttribute("error", "Such username already registered!");
             return "register";
         }
 
-        User user = this.userService.convertToEntity(userRegisterDTO);
+        User user = this.userService.convertToEntity(userRegForm);
         this.userService.register(user);
 
         return "redirect:login";
@@ -102,6 +120,29 @@ public class UserController {
         System.out.println("New email: " + editProfileDTO.getEmail());
         System.out.println("New pass: " + editProfileDTO.getPassword());
         return "edit-profile";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletResponse response){
+        final Cookie cookie = new Cookie("username","");
+        response.addCookie(cookie);
+        return "redirect:login";
+    }
+
+    //Model attributes
+    @ModelAttribute(name = "userRegForm")
+    public UserRegisterDTO initUserRegFormDto(){
+        return new UserRegisterDTO();
+    }
+
+    //Model Attributes
+    @ModelAttribute(name = "user")
+    public User getLoggedUser(@CookieValue(name = "username",defaultValue = "")
+                                          String username){
+        if (username.equals("")){
+            return null;
+        }
+        return this.userService.getUserByUsername(username);
     }
 
 }
