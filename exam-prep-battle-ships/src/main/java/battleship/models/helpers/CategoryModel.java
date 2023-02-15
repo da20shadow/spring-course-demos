80c1ce3e9816ab8ad772d3638c4661f4afad2e05
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-15 09:48:56 +0200

    create category model

---
+++ exam-prep-battle-ships/src/main/java/battleship/models/helpers/CategoryModel.java
@@ -0,0 +1,15 @@
+package battleship.models.helpers;
+
+import battleship.models.enums.ShipCategory;
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CategoryModel {
+    private Long id;
+    private ShipCategory name;
+    private String description;
+}
