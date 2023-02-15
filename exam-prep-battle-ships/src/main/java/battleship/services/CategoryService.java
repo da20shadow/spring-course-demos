913f18791fe1eea490571a46369438171edbce80
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-15 09:49:40 +0200

    Created services

---
+++ exam-prep-battle-ships/src/main/java/battleship/services/CategoryService.java
@@ -0,0 +1,38 @@
+package battleship.services;
+
+import battleship.models.helpers.CategoryModel;
+import battleship.models.entities.Category;
+import battleship.models.enums.ShipCategory;
+import battleship.repositories.CategoryRepository;
+import jakarta.annotation.PostConstruct;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public CategoryService(CategoryRepository categoryRepository, ModelMapper modelMapper) {
+        this.categoryRepository = categoryRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    @PostConstruct
+    private void postConstruct(){
+        if (this.categoryRepository.count() == 0) {
+            this.categoryRepository.saveAllAndFlush(Arrays.stream(ShipCategory.values())
+                    .map(enumValue -> CategoryModel.builder()
+                            .name(enumValue)
+                            .description(enumValue + " description...")
+                            .build())
+                    .map(categoryModel -> this.modelMapper.map(categoryModel, Category.class))
+                    .toList());
+        }
+    }
+}
