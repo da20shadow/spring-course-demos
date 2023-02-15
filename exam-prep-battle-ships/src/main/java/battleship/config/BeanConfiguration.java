bc89396516146a21e6c4c6ccb750492f30a56790
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-15 09:48:33 +0200

    Create bean config

---
+++ exam-prep-battle-ships/src/main/java/battleship/config/BeanConfiguration.java
@@ -0,0 +1,20 @@
+package battleship.config;
+
+import battleship.models.helpers.LoggedUser;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfiguration {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public LoggedUser loggedUser() {
+        return new LoggedUser();
+    }
+}
