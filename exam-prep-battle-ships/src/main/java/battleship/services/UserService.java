77da0f68dd749f657e498771feae24401c360103
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-15 19:08:51 +0200

    Created user repository and service

--- exam-prep-battle-ships/src/main/java/battleship/services/UserService.java
+++ exam-prep-battle-ships/src/main/java/battleship/services/UserService.java
@@ -1,16 +1,29 @@
 package battleship.services;
 
+import battleship.models.UserModel;
+import battleship.models.entities.User;
 import battleship.repositories.UserRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, ModelMapper modelMapper) {
         this.userRepository = userRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    public UserModel getUserByUsername(String username) {
+        return this.modelMapper.map(this.userRepository.findByUsername(username).orElse(new User()), UserModel.class);
+    }
+
+    public UserModel getUserByEmail(String email) {
+        return this.modelMapper.map(this.userRepository.findByUsername(email).orElse(new User()), UserModel.class);
     }
 
 }
